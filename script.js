2d4a32d0c317ed2469d6930bbdcbfc68e4b12aa0
Rohit Thakur <rt773694@gmail.com>
2026-07-19 02:21:06 +0530

    Update website

--- script.js
+++ script.js
@@ -27,21 +27,21 @@ function showInfo(skill){
     const info = document.getElementById("skill-info");
 
     if(skill === "html"){
-        info.innerText = "HTML is used to create the structure of web pages.";
+        info.innerText = "HTML is a markup language used to structure content on web pages. It uses tags (like <div>, <p>, <img>) to define elements such as headings, paragraphs, images, links, and forms, which browsers render as visible pages.";
     }
     else if(skill === "css"){
-        info.innerText = "CSS is used to style and design web pages.";
+        info.innerText = " CSS is a stylesheet language used to style HTML elements — controlling colors, fonts, spacing, and layout. While HTML provides structure, CSS makes the website visually appealing and responsive across devices.";
     }
     else if(skill === "js"){
-        info.innerText = "JavaScript adds interactivity and dynamic behavior to websites.";
+        info.innerText = "JavaScript a programming language that adds interactivity to web pages, enabling features like button clicks, form validation, and animations. It runs directly in the browser and can manipulate the DOM; it's also used on the backend via Node.js.";
     }
     else if(skill === "react"){
-        info.innerText = "React is a JavaScript library used to build user interfaces.";
+        info.innerText = " React is a JavaScript library developed by Meta (Facebook) for building user interfaces. It follows a component-based architecture, breaking the UI into small, reusable pieces, and uses a Virtual DOM for fast, efficient rendering.";
     }
     else if(skill === "node"){
-        info.innerText = "Node.js allows JavaScript to run on the server side.";
+        info.innerText = "Node is a JavaScript runtime environment that allows JavaScript to run outside the browser, typically on the server side. Built on Chrome's V8 engine, it's non-blocking and event-driven, enabling developers to build backend servers, APIs, and database-connected applications using JavaScript.";
     }
     else if(skill === "mongodb"){
-        info.innerText = "MongoDB is a NoSQL database used to store application data.";
+        info.innerText = " Mongodb is a NoSQL database that stores data as JSON-like documents (in BSON format) instead of traditional tables and rows, using collections and documents. It offers a flexible schema, meaning each document's structure can vary, and is commonly used with Node.js in the MERN stack.";
     }
 }
